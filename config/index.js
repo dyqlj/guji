625dd65ed512347ecb5b9bbbf5ec07e2fc29ac4e
dyqlj <112696256+dyqlj@users.noreply.github.com>
2022-09-02 22:11:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxe1d34e7feb75fd2f",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "1cd34c9c3ff939f3ad7721ad88d7c7c0",
 
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "ekn1LRx0JNdvuXPo3QY08imBOpHB7WFMO_-44VLdm7I",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "",
@@ -24,14 +24,12 @@ export const config = {
      * ]
      */
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+        {name: '乖宝', id: "obmOd6YU6-yz1P02kmMRfEPl2OMI"}, 
+        {name: '宝宝', id: "obmOd6dwoQWXgHQ6dfrMJrbWflt8"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+        {name: '自己',id: "obmOd6dwoQWXgHQ6dfrMJrbWflt8"}, 
     ],
      
     /**
@@ -41,9 +39,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "安徽",
     // 所在城市
-    CITY: "惠州",
+    CITY: "六安",
 
     /** 重要节日相关 */
 
@@ -54,10 +52,10 @@ export const config = {
      * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
      */
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "生日", "name": "乖宝", "year": "2003", "date": "06-15"},
+        {"type": "节日", "name": "恋爱念日", "year": "2022", "date": "06-15"},
+        {"type": "生日", "name": "乖乖", "year": "2001", "date": "10-17"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2022", "date": "06-09"},
     ],
 
     /**
@@ -75,11 +73,11 @@ export const config = {
       * */
     CUSTOMIZED_DATE_LIST: [
       // 在一起的日子
-      {"keyword": "love_day", date: "2015-05-01"},
+      {"keyword": "love_day", date: "2022-06-15"},
       // 结婚纪念日
-      {"keyword": "marry_day", date: "2020-01-04"},
+      // {"keyword": "marry_day", date: "2020-01-04"},
       // 退伍日, 不用可以删掉
-      {"keyword": "ex_day", date: "2022-08-31"},
+      // {"keyword": "ex_day", date: "2022-08-31"},
       // sakana日
       // {"keyword": "sakana_day", date: "2022-01-06"},
       // ... 
